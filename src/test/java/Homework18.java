52925d90cb5da2d60dd1cb46b92fb3939d6b4d48
eolsen3 <137566317+eolsen3@users.noreply.github.com>
2023-08-17 12:01:38 -0600

    Update Homework 22
    Changes made to 18

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,7 +9,7 @@ public class Homework18 extends BaseTest {
 
     @Test
 public void playSong() throws InterruptedException {
-        navigateToPage();
+//       navigateToPage();
         provideEmail("erika.olsen@testpro.io");
         providePassword("te$t$tudent");
         clickSubmit();
@@ -16,7 +17,17 @@ public void playSong() throws InterruptedException {
         Assert.assertTrue(isSongPlaying());
     }
 
-public void clickPlay() {
+public void clickPlay() throws InterruptedException {
+    Thread.sleep(1000);    
+    
+    Actions actions3 = new Actions(driver);
+    WebElement elementToHover = driver.findElement(By.xpath("//*[@title= 'Play or resume' and @data-testid='play-btn']"));
+    WebElement elementToClick = driver.findElement(By.xpath("//*[@title='Play or resume' and @data-testid= 'play-btn']"));
+
+    actions3.moveToElement(elementToHover).perform();
+    elementToClick.click();
+    
+
     WebElement playNextButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//i[@data-testid='play-next-btn']")));
     WebElement playButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-testid='play-btn']")));
 
@@ -24,7 +35,8 @@ public void clickPlay() {
     playNextButton.click();
     playButton.click();
 }
-public boolean isSongPlaying() {
+public boolean isSongPlaying() throws InterruptedException {
+        Thread.sleep(5000);
     WebElement soundbar = driver.findElement(By.xpath("//div[@data-testid= 'sound-bar-play']"));
     return soundbar.isDisplayed();
 }
